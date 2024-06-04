9941fecaad56d353592b6252dc4a2710de86afe3
Daniil Belogur <katokinawa@icloud.com>
2024-06-04 23:16:42 +0300

    fix: remove clean-webpack-plugin & add clean: true option

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -9,7 +8,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
-    publicPath: ''
+    clean: true
   },
   devtool: "source-map",
   devServer: {
@@ -51,6 +50,5 @@ module.exports = {
       template: "./src/index.html",
     }),
     new MiniCssExtractPlugin(),
-    new CleanWebpackPlugin(),
   ],
 };
