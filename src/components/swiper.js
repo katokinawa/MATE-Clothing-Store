08a8c84a6bf494c19301b194146b2e5ac3326bb3
Daniil Belogur <katokinawa@icloud.com>
2024-06-27 22:55:03 +0300

    fix: adaptive for swiper with "hello"

--- src/components/swiper.js
+++ src/components/swiper.js
@@ -4,7 +4,6 @@ import Swiper from 'swiper/bundle';
 const swiper = new Swiper('.swiper', {
   wrapperClass: 'swiper__wrapper',
   spaceBetween: 0,
-  slidesPerView: 5,
   speed: 5000,
   allowTouchMove: false,
   loop: true,
@@ -13,6 +12,24 @@ const swiper = new Swiper('.swiper', {
     disableOnInteraction: true,
     reverseDirection: true,
   },
+  //1920px, 1440px, 1024px, 768px, 390px
+  breakpoints: {
+    390: {
+      slidesPerView: 3,
+    },
+    768: {
+      slidesPerView: 6,
+    },
+    1024: {
+      slidesPerView: 8,
+    },
+    1440: {
+      slidesPerView: 10,
+    },
+    1920: {
+      slidesPerView: 8,
+    },
+  }
 });
 
 let swiper_for_photo_slider = new Swiper('.swiper-photo-slider', {
