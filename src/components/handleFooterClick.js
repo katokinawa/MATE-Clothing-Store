5df2a17a97843e93d3cdf0a9af8c2939901259e7
Daniil Belogur <katokinawa@icloud.com>
2024-09-18 11:45:49 +0300

    feat: add footer info blocks

--- src/components/handleFooterClick.js
+++ src/components/handleFooterClick.js
@@ -1,12 +1,20 @@
 const footerBlocks = document.querySelectorAll(".footer__list-block-mobile");
+import arrowTop from '../img/svg/arrow-top.svg'
+import arrowBottom from '../img/svg/arrow-bottom.svg'
 
 export default function handleFooterClick() {
   function setEvent(elements) {
     elements.forEach((item) => {
       const footerList = item.querySelector(".footer__list");
       const footerButton = item.querySelector("p");
+      const footerIcon = item.querySelector(".footer__list-icon");
       footerButton.addEventListener("click", () => {
         toggleList(footerList);
+        footerIcon.classList.remove = 'footer__list-icon_arrow-top';
+      });
+      footerIcon.addEventListener("click", () => {
+        toggleList(footerList);
+        footerIcon.classList.remove = 'footer__list-icon_arrow-top';
       });
     });
   }
